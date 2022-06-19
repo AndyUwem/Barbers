852c3a00e5564c6745ebb10851f52132c94e729d
andyuwem <essienandy1993@gmail.com>
2022-06-19 21:50:07 +0100

    build transaction status page

--- src/app/components/apointment/apointment.module.ts
+++ src/app/components/apointment/apointment.module.ts
@@ -10,6 +10,8 @@ import { ApointmentPage } from './apointment.page';
 import { BackButtonPageModule } from 'src/app/reuseables/back-button/back-button.module';
 import { ApointmentListViewComponent } from './apointment-list-view/apointment-list-view.component';
 import { MyBarbersPageModule } from '../my-barbers/my-barbers.module';
+import { TransactionStatusComponent } from 'src/app/reuseables/transaction-status/transaction-status.component';
+import { TransactionStatusModule } from 'src/app/reuseables/transaction-status/transaction-status.module';
 
 @NgModule({
   imports: [
@@ -17,10 +19,11 @@ import { MyBarbersPageModule } from '../my-barbers/my-barbers.module';
     FormsModule,
     IonicModule,
     ApointmentPageRoutingModule,
+    TransactionStatusModule,
     BackButtonPageModule,
     MyBarbersPageModule
   ],
   declarations: [ApointmentPage, ApointmentListViewComponent],
-  entryComponents: [ApointmentListViewComponent]
+  entryComponents: [ApointmentListViewComponent, TransactionStatusComponent]
 })
 export class ApointmentPageModule {}
--- src/app/reuseables/transaction-status/transaction-status.module.ts
+++ src/app/reuseables/transaction-status/transaction-status.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+
+import { IonicModule } from '@ionic/angular';
+
+import { BackButtonPageModule } from '../back-button/back-button.module';
 import { TransactionStatusComponent } from './transaction-status.component';
 
 
 @NgModule({
   declarations: [TransactionStatusComponent],
-  exports: [TransactionStatusComponent]
+  exports: [TransactionStatusComponent],
+  imports: [IonicModule, BackButtonPageModule]
 })
 
 export class TransactionStatusModule {}
