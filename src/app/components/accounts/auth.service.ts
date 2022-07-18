1975e74d292506e788aa4a8d4fefd53cded6146b
andyuwem <essienandy1993@gmail.com>
2022-07-18 01:05:19 +0100

    set up user authentication

--- src/app/components/accounts/auth.service.ts
+++ src/app/components/accounts/auth.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
 
+import { LoginData } from 'src/app/interface/login-data.interface';
+
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut
+} from '@angular/fire/auth';
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService{
 
-      constructor(){}
+      constructor( private readonly auth: Auth ){}
 
-      login(email: string, password: string){
-          // signInWithEmailAndPassword(email, password)
+      login({ email, password }: LoginData ){
+         return signInWithEmailAndPassword(this.auth, email, password);
       }
+
+      registerUser({ email, password }: LoginData ){
+        return createUserWithEmailAndPassword(this.auth, email, password);
+     }
+
+     signOutUser(){
+          signOut(this.auth);
+     }
 }
